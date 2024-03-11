24af69c05bde5233d445075e53636fdb88b9b5be
GuillaumeChk <guillaume.chekhab39@gmail.com>
2024-03-11 20:12:47 +0100

    new build

--- dist/spa/assets/BookingPage.f0b97e85.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.0a2211dc.js";import{_ as a,I as r,J as t,K as o,N as s}from"./index.7efa3ac0.js";import"./render.2f7afced.js";const c={};function f(n,_){return r(),t(e,{class:"flex flex-center"},{default:o(()=>[s(" Tarifs et R\xE9servation ")]),_:1})}var m=a(c,[["render",f]]);export{m as default};
--- dist/spa/assets/DescriptionPage.8f5fa3bc.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.0a2211dc.js";import{_ as t,I as a,J as r,K as c,N as o}from"./index.7efa3ac0.js";import"./render.2f7afced.js";const s={};function n(f,_){return a(),r(e,{class:"flex flex-center"},{default:c(()=>[o(" Pr\xE9sentation du chalet ")]),_:1})}var x=t(s,[["render",n]]);export{x as default};
